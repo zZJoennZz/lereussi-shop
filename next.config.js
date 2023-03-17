c89ff8a3a69021ba5ae8ecc9dfd035848728f409
Joenn Aquilino <zzjoennzz@gmail.com>
2023-03-17 10:21:06 +0800

    New: Whitelisted backend url for image

--- next.config.js
+++ next.config.js
@@ -12,6 +12,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'upload.wikimedia.org',
       },
+      {
+        protocol: 'https',
+        hostname: 'dawnbringer.lereussicakes.com',
+      },
     ],
   },
   output: 'standalone',
